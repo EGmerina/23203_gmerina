5797efed4aefb9e304b7b6db6a9398601a913c21
EGmerina <e.gmerina@g.nsu.ru>
2025-01-19 19:48:08 +0000

    life resize is correct

--- GameOfLife/drawnspace.cpp
+++ GameOfLife/drawnspace.cpp
@@ -1,15 +1,16 @@
 #include "drawnspace.h"
 
-
+#define GAP_RATIO 0.93
 
 void DrawnSpace:: drawSpace(Space * space, QGraphicsScene* scene){
 
-    this->drawnspace.clear();
-    size_t distbtwcells = (scene->width()/space->getSize())*0.9;
-    size_t r = distbtwcells*0.8;
+    drawnspace.clear();
+    size_t sizeofscene = (scene->width() < scene->height())? scene->width(): scene->height();
+    qreal distbtwcells = (sizeofscene/static_cast<qreal>(space->getSize()));
+    qreal r = distbtwcells * GAP_RATIO;
     for(size_t y=0; y< space->getSize(); ++y){
         for(size_t x=0; x< space -> getSize(); ++x){
-            std:: unique_ptr<Circle> c = std::make_unique<Circle>(distbtwcells*y + distbtwcells/2,  distbtwcells*x + distbtwcells/2,  r, scene);
+            std:: unique_ptr<Circle> c = std::make_unique<Circle>(distbtwcells*x ,  distbtwcells*y ,  r, scene);
             c->setSpace(space);
             c->setPosition(x, y);
             if(space->isCellAlive(x, y)){
@@ -18,7 +19,7 @@ void DrawnSpace:: drawSpace(Space * space, QGraphicsScene* scene){
 
                 c->setBrush( QBrush(Qt::gray) );
             }
-            this->drawnspace.push_back(std::move(c));
+            drawnspace.push_back(std::move(c));
         }
     }
 }
