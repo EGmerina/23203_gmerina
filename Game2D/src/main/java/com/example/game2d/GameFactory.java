5d54c426cf13388e4adc62483dbacd99db7c7aca
EGmerina <e.gmerina@g.nsu.ru>
2025-04-30 02:13:06 +0000

    game2d with ok tree collission

--- Game2D/src/main/java/com/example/game2d/GameFactory.java
+++ Game2D/src/main/java/com/example/game2d/GameFactory.java
@@ -25,77 +25,39 @@ import static com.example.game2d.GameType.*;
 
 public class GameFactory implements EntityFactory {
 
-//    @Spawns("56")
-//    public Entity newBlock(SpawnData data) {
-//        var rect = new Rectangle(38, 38, Color.BLACK);
-//        rect.setArcWidth(15);
-//        rect.setArcHeight(15);
-//        rect.setStrokeWidth(1);
-//        rect.setStroke(Color.BLUE);
-//
-//        return entityBuilder(data)
-//                .type(BLOCK)
-//                .viewWithBBox(rect)
-//                .zIndex(-1)
-//                .build();
-//    }
-//
-//    @Spawns("35")
-//    public Entity newCoin(SpawnData data) {
-//
-//        return entityBuilder(data)
-//                .type(COIN)
-//                .bbox(new HitBox(new Point2D(5, 5), BoundingShape.box(30, 30)))
-//                .zIndex(-1)
-//                .with(new CollidableComponent(true))
-//                .with(new CellMoveComponent(BLOCK_SIZE, BLOCK_SIZE, 50))
-//                .scale(0.5, 0.5)
-//                .build();
-//    }
 
     @Spawns("player")
     public Entity newPlayer(SpawnData data) {
-        //AnimatedTexture view = texture("Artyom.png").toAnimatedTexture(16, Duration.seconds(0.33));
-
         return entityBuilder(data)
                 .type(PLAYER)
-                .bbox(new HitBox(new Point2D(4, 4), BoundingShape.box(32, 32)))
+                .bbox(new HitBox(new Point2D(4, 4), BoundingShape.box(16, 32)))
                 .anchorFromCenter()
-                // .view(view.loop())
                 .with(new CollidableComponent(true))
                 .with(new CellMoveComponent(BLOCK_SIZE, BLOCK_SIZE, 200).allowRotation(false))
-                // there is no grid constructed yet, so pass lazily
                 .with(new AStarMoveComponent(new LazyValue<>(() -> geto("grid"))))
                 .with(new PlayerComponent())
-                .rotationOrigin(35 / 2.0, 40 / 2.0)
                 .build();
     }
 
     @Spawns("runner")
     public Entity newRunner(SpawnData data) {
-        // AnimatedTexture view = texture("player.png").toAnimatedTexture(2, Duration.seconds(0.33));
-
-        return entityBuilder(data)
+       return entityBuilder(data)
                 .type(RUNNER)
-                .bbox(new HitBox(new Point2D(4, 4), BoundingShape.box(32, 32)))
+                .bbox(new HitBox(new Point2D(4, 4), BoundingShape.box(16, 32)))
                 .anchorFromCenter()
-                // .view(view.loop())
                 .with(new CollidableComponent(true))
                 .with(new CellMoveComponent(BLOCK_SIZE, BLOCK_SIZE, 100).allowRotation(false))
-                // there is no grid constructed yet, so pass lazily
                 .with(new AStarMoveComponent(new LazyValue<>(() -> geto("runnerGrid"))))
                 .with(new RunnerComponent())
-                .rotationOrigin(35 / 2.0, 40 / 2.0)
                 .build();
     }
 
     @Spawns("tree")
     public Entity newTree(SpawnData data) {
-        Texture view = texture("tree_2.png");
         return entityBuilder(data)
                 .type(TREE)
                 .with(new CollidableComponent(true))
-                .bbox(new HitBox(new Point2D(10, 10), BoundingShape.box(10, 30)))
+                .bbox(new HitBox(new Point2D(32, 40), BoundingShape.box(15, 35)))
                 .anchorFromCenter()
                 .zIndex(100)
                 .build();
@@ -110,11 +72,5 @@ public class GameFactory implements EntityFactory {
                 .bbox(new HitBox(BoundingShape.box(width, height)))
                 .build();
     }
-//
-//    @Spawns("waypoint")
-//    public Entity newWaypoint(SpawnData data) {
-//        return entityBuilder(data)
-//                .type(WAY)
-//                .build();
-//    }
+
 }
